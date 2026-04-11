6abbc315781ecd928b545ac80c0c1ac9c873fb67
Shibu_01 <shibuy0011@gmail.com>
2026-04-11 22:48:26 +0530

    Initial commit

---
+++ src/main/java/com/project_fitness/FitnessMonolitsApplication.java
@@ -0,0 +1,13 @@
+package com.project_fitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessMonolitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessMonolitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project_fitness/FitnessMonolitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_fitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessMonolitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
